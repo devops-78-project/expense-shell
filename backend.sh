642872fbbdc79c2680e40f97aedec7045a7ee561
ganesh <desaiganeshkumar13@gmail.com>
2024-03-03 22:37:22 +0530

    added step 6 by removing old content in backend.sh file

--- backend.sh
+++ backend.sh
@@ -2,6 +2,8 @@ dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
 cp backend.service /etc/systemd/system/backend.service
+
+rm -rf /app
 useradd expense
 mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
