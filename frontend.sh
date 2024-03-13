ffacac5c0e4d3a1f70f34812d3d766a2444db963
ganesh <desaiganeshkumar13@gmail.com>
2024-03-13 11:00:00 +0530

    added check status and print heading task in frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -1,17 +1,31 @@
 source common.sh
+
+print_heading_task "install nginx"
 dnf install nginx -y
+check status $?
 
-
+print_heading_task "enable nginx"
 systemctl enable nginx
 systemctl start nginx
+check status $?
 
+print_heading_task "copy expenseservice.conf"
 cp expenseservice.conf /etc/nginx/default.d/expense.conf
+check status $?
 
+print_heading_task "clean old content"
 rm -rf /usr/share/nginx/html/*
+check status $?
 
+print_heading_task "install app content"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+check status $?
 
+print_heading_task "extract app content"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+check status $?
 
+print_heading_task "restart nginx"
 systemctl restart nginx
+check status $?
