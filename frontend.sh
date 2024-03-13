cc115de5ad74f717be9e1554cfe4632737c67956
ganesh <desaiganeshkumar13@gmail.com>
2024-03-13 11:13:47 +0530

    status changes are changed in frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -4,30 +4,30 @@ source common.sh
 
 print_heading_task "install nginx"
 dnf install nginx -y &>>$log
-check status $? &>>$log
+check status $?
 
 print_heading_task "enable nginx"
 systemctl enable nginx &>>$log
 systemctl start nginx &>>$log
-check status $? &>>$log
+check status $?
 
 print_heading_task "copy expenseservice.conf"
 cp expenseservice.conf /etc/nginx/default.d/expense.conf &>>$log
-check status $? &>>$log
+check status $?
 
 print_heading_task "clean old content"
 rm -rf /usr/share/nginx/html/* &>>$log
-check status $? &>>$log
+check status $?
 
 print_heading_task "install app content"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log
-check status $? &>>$log
+check status $?
 
 print_heading_task "extract app content"
 cd /usr/share/nginx/html &>>$log
 unzip /tmp/frontend.zip &>>$log
-check status $? &>>$log
+check status $?
 
 print_heading_task "restart nginx"
 systemctl restart nginx &>>$log
-check status $? &>>$log
+check status $?
