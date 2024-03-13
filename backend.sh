4fb7167f8ff5fa131fe8a05ddf9c1886e716a027
ganesh <desaiganeshkumar13@gmail.com>
2024-03-13 11:23:25 +0530

    status changes are changed in backend.sh

--- backend.sh
+++ backend.sh
@@ -13,87 +13,70 @@ fi
 
 print_heading_task "disable default old nodejs version"
 dnf module disable nodejs -y &>>$log
-
 check_status $?
 
 print_heading_task "enable new nodejs version"
 dnf module enable nodejs:20 -y &>>$log
-
 check_status $?
 
 
 print_heading_task "install nodejs"
 dnf install nodejs -y &>>$log
-
 check_status $?
 
 print_heading_task "add user application"
 useradd expense &>>$log
-
 check_status $?
 
 
 print_heading_task "copy backendservice conf file"
 cp backendservice.conf /etc/systemd/system/backend.service &>>$log
-
 check_status $?
 
 
 print_heading_task "clean the old content"
 rm -rf /app &>>$log
-
 check_status $?
 
 
 print_heading_task "create app directory"
 mkdir /app &>>$log
-
 check_status $?
 
 
 print_heading_task "download app content"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>$log
-
 check_status $?
 
 
 print_heading_task "extract app content"
 cd /app &>>$log
-
 unzip /tmp/backend.zip &>>$log
-
 check_status $?
 
 
 print_heading_task "download nodejs dependencies"
 cd /app &>>$log
-
 npm install &>>$log
-
 check_status $?
 
 
 print_heading_task "run daemon-reload"
 systemctl daemon-reload &>>$log
-
 check_status $?
 
 
 print_heading_task "start backend service"
 systemctl enable backend &>>$log
-
 systemctl start backend &>>$log
-
 check_status $?
 
 
 print_heading_task "install mysql"
 dnf install mysql -y &>>$log
-
 check_status $?
 
 
 print_heading_task "load schema"
 mysql -h 172.31.92.59 -uroot -p${root_user_password} < /app/schema/backend.sql &>>$log
-
 check_status $?
