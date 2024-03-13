8964d5f1eadee30934eacdd9fc39e0127afedb5e
ganesh <desaiganeshkumar13@gmail.com>
2024-03-13 11:35:06 +0530

    added check status and redirctors

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,20 @@
-dnf install mysql-server -y
+source common.sh
 
-systemctl enable mysqld
-systemctl start mysqld
+if  [ -z "${mysql_root_password}" ]; then
+  echo input password is missing
+  exit 1
 
-mysql_secure_installation --set-root-pass ExpenseApp@1
+fi
 
+print_heading_task "install mysql "
+dnf install mysql-server -y &>>$log
+check_status $?
+
+print_heading_task "enable mysql service"
+systemctl enable mysqld &>>$log
+systemctl start mysqld &>>$log
+check_status $?
+
+print_heading_task "setup root password"
+mysql_secure_installation --set-root-pass ${root_user_password} &>>$log
+check_status $?
